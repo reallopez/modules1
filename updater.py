a8cdb776435b32e8b2ee2b4a04cbd6232f8e1fcb
reallopez <lipeeditcontas13114@gmail.com>
2023-11-28 18:22:38 -0300

    Initial commit

---
+++ updater.py
@@ -0,0 +1,19 @@
+from requests import get
+from time import sleep
+from modules.functions import *
+from modules.variables import *
+import tkinter.messagebox
+import webbrowser
+
+def check_update():
+    """Checks for updates"""
+    change_title(f"Real Lopez")
+    print(f"    [{red}>{reset}] Procurando atualizações")
+    
+    try:
+        latest_version = get("https://raw.githubusercontent.com/l1pezim1/real_lopez/master/version").text.rstrip()
+    except:
+        print(f"    [{red}>{reset}] Could not connect to server")
+        sleep(2)
+        return
+    
